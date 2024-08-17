51e2c0317927672511fc700fa03fc8867ff45a6e
hrjugar <hernanjugar123@gmail.com>
2024-08-17 19:19:02 +0800

    Project creation modal successfully adds project to database

--- automated-electrical-schedule/Data/DatabaseContext.cs
+++ automated-electrical-schedule/Data/DatabaseContext.cs
@@ -24,37 +24,4 @@ public class DatabaseContext : DbContext
 
         modelBuilder.Entity<ConductorType>().HasData(ConductorTypeSeed.All);
     }
-
-    // public async Task InitAsync()
-    // {
-    //     if (_conn is not null) return;
-    //
-    //     _conn = new SQLiteAsyncConnection(DbPath);
-    //     await _conn.ExecuteAsync("PRAGMA foreign_keys = ON;");
-    //
-    //     if (!await IsTableExisting(ConductorType.TableName))
-    //     {
-    //         await _conn.CreateTableAsync<ConductorType>();
-    //         await _conn.InsertAllAsync(ConductorTypeData.All);
-    //     }
-    //
-    //     if (!await IsTableExisting(DistributionBoard.TableName)) await _conn.CreateTableAsync<DistributionBoard>();
-    // }
-    //
-    // private async Task<bool> IsTableExisting(string tableName)
-    // {
-    //     if (_conn is null) throw new InvalidOperationException("Database is not initialized.");
-    //
-    //     var tableCount = await _conn.ExecuteScalarAsync<int>(
-    //         "SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name = ?;", tableName
-    //     );
-    //
-    //     return tableCount > 0;
-    // }
-    //
-    // public async Task<List<ConductorType>> GetConductorTypesAsync()
-    // {
-    //     await InitAsync();
-    //     return await _conn!.Table<ConductorType>().ToListAsync();
-    // }
 }
